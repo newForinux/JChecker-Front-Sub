e899139e2689ea0807c2f44097e144cc0c882a99
newForinux <you_maybe@naver.com>
2021-03-16 02:49:26 +0900

    Modify part of property.

--- src/modules/components/AppbarLayout.tsx
+++ src/modules/components/AppbarLayout.tsx
@@ -1,8 +1,7 @@
 import MuiAppBar, { AppBarProps } from '@material-ui/core/AppBar';
-import React from "react";
 
 
 export default function AppBar(children: AppBarProps) {
 
-    return <MuiAppBar style={{alignItems: 'center'}} elevation={0} position="static" {...children} />;
+    return <MuiAppBar elevation={0} position="static" {...children} />;
 }
