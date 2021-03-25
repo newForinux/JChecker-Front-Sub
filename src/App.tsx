92fc5756ccef68d19d2a59e705aed7855d82667f
newForinux <you_maybe@naver.com>
2021-03-25 18:50:00 +0900

    Add error page.

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-d
 import Index from './Home';
 import PreClass from './modules/views/EntranceClass';
 import PreInstructor from './modules/views/EntranceInstructor';
-import EachClass from './modules/components/classes/SectionClass'
+import SectionClass from './modules/components/classes/SectionClass'
+import SectionClassForInst from './modules/components/classes/SectionClassForInst';
+import SectionError from './modules/components/classes/SectionError';
 
 //const theme = unstable_createMuiStrictModeTheme();
 
@@ -13,8 +15,10 @@ function App() {
       <Switch>
         <Route exact path="/" component={Index} />
         <Route exact path="/classes" component={PreClass} />
-        <Route exact path="/classes/:token" component={EachClass} />
+        <Route exact path="/classes/:token" component={SectionClass} />
         <Route exact path="/instructors" component={PreInstructor} />
+        <Route exact path="/instructors/:token" component={SectionClassForInst} />
+        <Route exact path="/error" component={SectionError} />
         <Redirect path="*" to="/" />
       </Switch>
     </Router>
