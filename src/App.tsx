a68500acbbafcaba6028a4b9abd2cb755a71a3f4
newForinux <you_maybe@naver.com>
2021-04-28 20:53:25 +0900

    Change components name

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
 import Index from './Home';
-import PreClass from './modules/views/EntranceClass';
-import PreInstructor from './modules/views/EntranceInstructor';
+import EntranceClass from './modules/views/EntranceClass';
+import EntranceInstructor from './modules/views/EntranceInstructor';
 import SectionClass from './modules/components/classes/SectionClass'
 import SectionClassForInst from './modules/components/classes/SectionClassForInst';
 import SectionError from './modules/components/classes/SectionError';
@@ -15,10 +15,10 @@ function App() {
     <Router>
       <Switch>
         <Route exact path="/jchecker" component={Index} />
-        <Route exact path="/jchecker/classes" component={PreClass} />
+        <Route exact path="/jchecker/classes" component={EntranceClass} />
         <Route exact path="/jchecker/classes/:token" component={SectionClass} />
         <Route exact path="/jchecker/classes/:token/success" component={ResultProcess} />
-        <Route exact path="/jchecker/instructors" component={PreInstructor} />
+        <Route exact path="/jchecker/instructors" component={EntranceInstructor} />
         <Route exact path="/jchecker/instructors/:token" component={SectionClassForInst} />
         <Route exact path="/jchecker/error" component={SectionError} />
         <Redirect path="*" to="/jchecker" />
